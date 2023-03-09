21ff3236d08ced68b2066a21d6e7b86f9a4e3654
Shahid Islam <codershahid@icloud.com>
2023-03-09 09:21:32 +0530

    nitialization of new react app with tailwind configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <div className="text-center text-2xl uppercase">
+    welcome to travelopia app
+  </div>
+);
